78c2b0fa88b29eb64c1c41adee13bfa09e672f80
JJ <jj.vega86@gmail.com>
2022-09-11 16:15:42 -0400

    Added modal component based on React Modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 // Components
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
+import CustomModal from "./components/CustomModal";
 
 function App() {
   return (
     <>
       <NavBar />
+      <CustomModal buttonLabel="Open Sesame">
+        <h1>Some Heading</h1>
+        <p>Some label</p>
+      </CustomModal>
       <Header displayText="Video Games - Analyzed" />
     </>
   );
---
+++ src/components/CustomModal.jsx
@@ -0,0 +1,40 @@
+import Modal from "react-modal";
+import { useState } from "react";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+export default function CustomModal({ children, buttonLabel }) {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <button onClick={openModal}>{buttonLabel}</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <button onClick={closeModal}>close</button>
+        {children}
+      </Modal>
+    </div>
+  );
+}
