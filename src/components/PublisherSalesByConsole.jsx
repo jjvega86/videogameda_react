8f22fdcad5a641a6660fb6d33fc84b107b0dcfc9
Megan Cindric <megancindric@gmail.com>
2022-09-21 18:22:54 -0500

    👌 IMPROVE: Added loading spinner to chart, switched bar chart to vertical orientation

--- src/components/PublisherSalesByConsole.jsx
+++ src/components/PublisherSalesByConsole.jsx
@@ -1,44 +1,65 @@
+// React
 import { useState, useEffect } from "react";
+
+// API Call
 import { getPublisherConsoleSalesQuery } from "../api/videoGameApi";
-import GameConsoleSelector from "./GameConsoleSelector";
+
+// Packages
 import { Chart } from "react-google-charts";
+import CircularProgress from "@mui/material/CircularProgress";
+
+// Components
+import GameConsoleSelector from "./GameConsoleSelector";
 
 export default function PublisherSalesByConsole() {
   const [gameConsole, setGameConsole] = useState("DS");
   const [publisherSales, setPublisherSales] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const options = {
-    title: `Top 10 Publisher Sales on ${gameConsole}`,
-    chartArea: { width: "50%" },
-    vAxis: {
-      title: "Publisher",
-    },
-    hAxis: {
-      title: "Sales (millions)",
+    chart: {
+      title: `Top 10 Publishers for the ${gameConsole}`,
+      subtitle: "Based on total global sales",
     },
   };
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       let data = await getPublisherConsoleSalesQuery(gameConsole);
       data.unshift(["Publisher", "Global Sales (Millions)"]);
       setPublisherSales(data);
+      setLoading(false);
     };
     fetchData();
   }, [gameConsole]);
   return (
     <>
-      {console.log("Publisher Sales: ", publisherSales)}
       <h3>Top 10 Publisher Sales for the {gameConsole}</h3>
       <GameConsoleSelector
         gameConsole={gameConsole}
         setGameConsole={setGameConsole}
-      />
-      <Chart
-        chartType="BarChart"
-        data={publisherSales}
-        options={options}
-        width="100%"
-        height="400px"
-      />
+      />{" "}
+      <div
+        style={{
+          width: "100%",
+          height: "450px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <Chart
+            chartType="Bar"
+            data={publisherSales}
+            options={options}
+            width="100%"
+            height="400px"
+          />
+        )}{" "}
+      </div>
     </>
   );
 }
