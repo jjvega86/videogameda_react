b98fac886572aa6d8034d6ce2aed9ac550dca2aa
Megan Cindric <megancindric@gmail.com>
2022-09-20 19:46:16 -0500

    📦 NEW: Created component for visualizing publisher sales by console

---
+++ src/components/PublisherSalesByConsole.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+export default function PublisherSalesByConsole() {
+  const [console, setConsole] = useState("Nintendo");
+  const [publisherSales, setPublisherSales] = useState([]);
+  return (
+    <>
+      <h3>Publisher Sales by Console!</h3>
+    </>
+  );
+}
